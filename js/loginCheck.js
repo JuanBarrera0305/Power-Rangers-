557fb350bc75db2816be9a433a053110d5b45d3e
craigPulfEdu <122327947+craigPulfEdu@users.noreply.github.com>
2023-02-21 13:16:22 -0800

    upload

---
+++ js/loginCheck.js
@@ -0,0 +1,12 @@
+// author: craig pulford
+// checks artifical login list
+
+function loginCheck(username,password) {
+    for (let i = 0; i < usernames.length;i++) {
+        
+    }
+}
+
+var usernames=["craig","username","samuel","juan","user","hackerman"]
+
+var password=["pulford","password","fehl","barrera","name","hackers"]
