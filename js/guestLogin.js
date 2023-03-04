48b96a2aa03daf889c88fba809fab7b5023415e6
craigPulfEdu <122327947+craigPulfEdu@users.noreply.github.com>
2023-03-04 12:23:30 -0800

    added login check
    login check is semi working, just need to fix the loop so it doesn't repeat 5 times

---
+++ js/guestLogin.js
@@ -0,0 +1,2 @@
+// author: craig Pulford
+// allows one to view menu or order as a guest
--- js/loginCheck.js
+++ js/loginCheck.js
@@ -3,10 +3,19 @@
 
 function loginCheck(username,password) {
     for (let i = 0; i < usernames.length;i++) {
-        
+        if (username === usernames[i]) {
+            if (password === passwords[i]) {
+                window.location.href = "ccpulfo.it.pointpark.edu/menu.html";
+            } else if (i > usernames.length) 
+                alert("Incorrect Password");
+        } else { 
+            alert("Username does not exist");
+        }
     }
 }
 
+// make sure usernames array is the same size as passwords array
+
 var usernames=["craig","username","samuel","juan","user","hackerman"]
 
-var password=["pulford","password","fehl","barrera","name","hackers"]
+var passwords=["pulford","password","fehl","barrera","name","hackers"]
