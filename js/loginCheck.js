d10cb8cc7411c5b4a35fad2246f517aa4e947005
craigPulfEdu <122327947+craigPulfEdu@users.noreply.github.com>
2023-03-06 10:25:01 -0800

    login check mostly works now
    there is a slight error where a double error occurs will fix later

--- js/loginCheck.js
+++ js/loginCheck.js
@@ -1,18 +1,30 @@
 // author: craig pulford
 // checks artifical login list
 
-function loginCheck(username,password) {
-    for (let i = 0; i < usernames.length;i++) {
-        if (username === usernames[i]) {
-            if (password === passwords[i]) {
-                window.location.href = "menu_Folder/HomeMenu.html";
-            } else if (i > usernames.length) 
-                alert("Incorrect Password");
-        } else { 
+function usernameCheck (username,password) {
+    switch (username) {
+        case "craig":
+        case "username":
+        case "samuel":
+        case "juan":
+        case "user":
+        case "hackerman":
+            checkpassword(username,password);
+        default:
             alert("Username does not exist");
-        }
+            break;
     }
 }
+
+function checkpassword(username,password) {
+    var checkNum = usernames.indexOf(username);
+    if (passwords[checkNum] === password)
+        window.location.href = "menu_Folder/HomeMenu.html";
+    else {
+        alert("Incorrect Password")
+    }
+}
+
 
 
 // make sure usernames array is the same size as passwords array
