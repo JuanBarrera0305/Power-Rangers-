7def3e38f81187f4b8a4d93f554acfe5ac67d00b
craigPulfEdu <122327947+craigPulfEdu@users.noreply.github.com>
2023-03-10 13:22:03 -0500

    klnmlkn

--- js/guestLogin.js
+++ js/guestLogin.js
@@ -1,8 +1,13 @@
 // author: craig Pulford
 // allows one to view menu or order as a guest
-// This login should prompt and ask for a Name, Phone Number
+// This login should prompt and ask for a Name
+
+// Potentially in future make this function have a boolean like Guest == true so later at checkout they have to enter an address or some
+// piece of identifying information or it can be asked here and saved for later instead 
+
+var guestUser
 
 function guestLogin() {
-    prompt("What is your name?");
+    guestUser = prompt("What is your name?","Enter Your Name");
     window.location.href="menu_folder/HomeMenu.html";
 }
